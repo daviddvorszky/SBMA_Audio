e402010b9fc60b72c93f8680116dba9c83e9eb0c
xR3l4Xx <halloka.mangoka@gmail.com>
2022-09-30 23:11:07 +0300

    Add play audio function

--- app/src/main/java/com/example/sbma_audio/MainActivity.kt
+++ app/src/main/java/com/example/sbma_audio/MainActivity.kt
@@ -2,22 +2,37 @@ package com.example.sbma_audio
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.AudioAttributes
+import android.media.AudioFormat
+import android.media.AudioTrack
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.sbma_audio.ui.theme.SBMA_AudioTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.io.InputStream
+import java.time.LocalTime
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var inputStream: InputStream
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        hasPermission()
+
         setContent {
             SBMA_AudioTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,9 +41,62 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Text("Audio Lab")
+                    Button(onClick = {
+                        GlobalScope.launch(Dispatchers.Main) {
+                            inputStream = resources.openRawResource(R.raw.when_you_try_your_best)
+                            async(Dispatchers.Default) { playAudio(inputStream) }
+                        }
+                    }) {
+                        Text("Play Audio")
+                    }
                 }
             }
         }
+
+
+    }
+
+    private fun playAudio(istream: InputStream) {
+        val minBufferSize = AudioTrack.getMinBufferSize(
+            44100, AudioFormat.CHANNEL_OUT_STEREO,
+            AudioFormat.ENCODING_PCM_16BIT
+        )
+        val aBuilder = AudioTrack.Builder()
+        val aAttr: AudioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build()
+        val aFormat: AudioFormat = AudioFormat.Builder()
+            .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+            .setSampleRate(44100)
+            .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
+            .build()
+        val track = aBuilder.setAudioAttributes(aAttr)
+            .setAudioFormat(aFormat)
+            .setBufferSizeInBytes(minBufferSize)
+            .build()
+        track.setVolume(1f)
+
+        track.play()
+        var i = 0
+        val buffer = ByteArray(minBufferSize)
+        try {
+            i = istream.read(buffer, 0, minBufferSize)
+            while(i != -1){
+                track.write(buffer, 0, i)
+                i = istream.read(buffer, 0, minBufferSize)
+            }
+        }catch (e: IOException){
+            Log.e("pengb", "Stream read error $e")
+        }
+        try {
+            istream.close()
+        }catch (e: IOException){
+            Log.e("pengb", "Close error $e")
+        }
+
+        track.stop()
+        track.release()
     }
 
     private fun hasPermission(): Boolean {
